e4250bcd92e48992a4e5b1d32e29dbd41083813a
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-04-13 02:43:35 -0400

    a9 MONGODB sp22

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/webdev');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+//mongoose.connect('mongodb://127.0.0.1:27017/webdev');
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors());
